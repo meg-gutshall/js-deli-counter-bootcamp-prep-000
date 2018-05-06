fd4ba040060590db873d46af6263323d2782c987
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-05-06 13:46:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeli, otherDeli) {
+  katzDeliLine.shift();
   katzDeli.push(otherDeli);
   katzDeliLine.push("Welcome, " + otherDeli + ". You are number " + katzDeli.length + " in line.");
   return katzDeliLine;
-  
 }
