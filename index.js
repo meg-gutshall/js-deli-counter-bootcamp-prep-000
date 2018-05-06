6437d49d184102916dcf2ec65d82b1c3d8bcfdf5
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-05-06 14:23:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty.";
   } else {
-    for (let i=line.length;) {
+    while (line.length;) {
       
     }
   }
