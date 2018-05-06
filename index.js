8fb4f8dcc5b360197966348f224d8e87331495b6
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-05-06 14:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ function currentLine(line) {
   } else {
     var order = [];
     do {
-      order.unshift(line.length + ". " + console.log(line[line.length]) + ", ");
+      order.unshift(line.length + ". " + console.log(line[(line.length - 1)]) + ", ");
       --line.length;
-    } while (line.length > -1);
+    } while (line.length > 0);
     return "The line is currently: " + order;
   }
 }
