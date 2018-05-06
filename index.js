0361646ddd8b32af6ec368e2e3335821b786d683
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-05-06 13:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var katzDeli = [];
 
-var katzDeliLine = otherDeli.length;
-
 function takeANumber(katzDeliLine, otherDeli) {
-  for (let i = katzDeliLine.length; i > 0; --i) {
+  for (let i = otherDeli.length; i > 0; --i) {
     katzDeli.push("Welcome, " + otherDeli[i] + ". You are number " + katzDeliLine[i] + " in line.");
   }
   return katzDeli;
